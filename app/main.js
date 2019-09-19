fe5cf6320c1760e57d06e8c7a976a2454d211696
Ammar Razzak <razzakammaredu@gmail.com>
2019-09-18 22:01:27 -0400

    Added Path

--- app/main.js
+++ app/main.js
@@ -90,53 +90,73 @@ document.querySelector('#setAlarm').addEventListener('click', function () {
   if (AlarmValue === '') {
     alert('Fix your alarm')
   } else {
-    Alarm();
+    Alarm(AlarmValue);
   }
 
 });
 
+// First Wake Up
+function firstWake() {
+  audio = new Audio(`./assests/Get_up_rogers.mp3`)
+  audio.play();
+  audio.addEventListener('ended', function () {
+    newAudio = new Audio('./assests/How_did_you_sleep_1.mp3');
+    newAudio.play();
 
+    newAudio.addEventListener('ended', function () {
+      recognition.start();
+      recognition.onresult = function (event) {
+        const current = event.resultIndex;
 
-// Alarm Functionality 
-// Set the date we're counting down to
-// function Alarm() {
-//   var countDownDate = new Date("Sep 18, 2019 20:53:40").getTime();
-//   // Get today's date and time
-//   var now = new Date().getTime();
+        const transcript = event.results[current][0].transcript;
 
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//   // // Display the result in the element with id="demo"
-//   // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//   //   + minutes + "m " + seconds + "s ";
-
-//   // If the count down is finished, write some text
-//   if (distance < 0) {
-//     console.log('FInished')
-//     alert('EXPIRED')
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-// }
-
-// Alarm Function
-function Alarm() {
-  var countDownDate = new Date("Sep 18, 2019 21:00:00").getTime();
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  if (countDownDate < now) {
-    document.getElementById('demo').innerHTML = 'EXPIRED'
+        if (transcript.includes('good' || 'great' || 'really well' || 'amazing')) {
+          greatAudio = new Audio('./assests/Positive.mp3');
+          greatAudio.play();
+        } else {
+          var randomValue = notUnderstand[Math.floor(Math.random() * notUnderstand.length)];
+          audio = new Audio(randomValue);
+          audio.play();
+        }
+      }
+    })
   }
+  )
 }
 
-Alarm();
+document.getElementById('goesoff').addEventListener('click', function () {
+  firstWake();
+})
+
+// Alarm
+// Set the date we're counting down to
+function Alarm(AlarmValue) {
+  var countDownDate = new Date(`${AlarmValue}`).getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("timeRemain").innerHTML = "Time Remaining: " + days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      firstWake();
+      console.log('Finished')
+      clearInterval(x);
+    }
+  }, 1000);
+}
